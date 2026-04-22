28abbed68f8fb6279f418c901cb5bab21f1ca981
ALINE ALVES DE SOUZA <25132561@dsn127m1242314.cfp127aluno.local>
2026-04-22 11:15:37 -0300

    feat: Adiciona tabelas do banco de dados, ainda não completo

--- scripts/espectra_tables.sql
+++ scripts/espectra_tables.sql
@@ -8,6 +8,7 @@ USE db_espectra;
 CREATE TABLE tb_responsavel(
 
 	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    foto VARCHAR(255) NOT NULL,
     nome VARCHAR(150) NOT NULL,
     data_nascimento DATE NOT NULL,
     telefone VARCHAR(20) NOT NULL,
@@ -19,6 +20,7 @@ CREATE TABLE tb_responsavel(
 CREATE TABLE tb_psicopedagogo(
 
 	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    foto VARCHAR(255) NOT NULL,
     nome VARCHAR(150) NOT NULL,
     data_nascimento DATE NOT NULL,
     telefone VARCHAR(20) NOT NULL,
@@ -73,21 +75,118 @@ CREATE TABLE tb_faixa_idade(
     idade_max INT NOT NULL
 );
 
-CREATE TABLE tb_tipo_aplicacao(
-	
-    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    alternativa varchar(40) NOT NULL
+
+
+CREATE TABLE tb_status_atividade(
+
+	id 	INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    status_atividade VARCHAR(20)
+    
 );
 
+CREATE TABLE tb_habilidade(
+	
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nome VARCHAR(20) NOT NULL
+
+);
+
+CREATE TABLE tb_atividade(
+	
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    
+    CONSTRAINT fk_habilidade
+    FOREIGN KEY (id_habilidade) REFERENCES tb_habilidade(id),
+    
+    CONSTRAINT fk_status_atividade
+    FOREIGN KEY (id_status_atividade)  REFERENCES tb_status_atividade(id),
+    
+    CONSTRAINT fk_paciente
+    FOREIGN KEY (id_paciente) REFERENCES tb_paciente(id)
+    
+    );
+    
+    
+	CREATE TABLE tb_tipo_aplicacao(
+	
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    alternativa varchar(30) NOT NULL
+);
 
 CREATE TABLE tb_tentativa(
 
 	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    observacao VARCHAR(300),
+    resultado BOOLEAN NOT NULL,
+    observacao VARCHAR(1500),
     
     CONSTRAINT fk_tipo_aplicacao
-    FOREIGN KEY (id_tipo_aplicacao)  REFERENCES tb_tipo_aplicacao(id)
+    FOREIGN KEY (id_tipo_aplicacao)  REFERENCES tb_tipo_aplicacao(id),
+    
+	CONSTRAINT fk_atividade
+    FOREIGN KEY (id_atividade)  REFERENCES tb_atividade(id)
 
 );
 
+CREATE TABLE tb_atividade_personalizada(
 
+	id 	INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    questao VARCHAR(300) NOT NULL,
+    valor_meses INT NOT NULL,
+    
+    CONSTRAINT fk_psicopedagogo
+    FOREIGN KEY (id_psicopedagogo)  REFERENCES tb_psicopedagogo(id),
+    
+	CONSTRAINT fk_atividade
+    FOREIGN KEY (id_atividade)  REFERENCES tb_atividade(id)
+    
+);
+
+
+CREATE TABLE tb_resposta_formulario(
+	
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    alternativa VARCHAR(20)
+);
+
+
+	CREATE TABLE tb_serie_escolar(
+		
+        id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        serie VARCHAR(10) NOT NULL
+    );
+    
+    CREATE TABLE tb_grau_suporte(
+		
+        id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        grau INT NOT NULL
+    );
+    
+    CREATE TABLE tb_paciente(
+		
+        id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        foto VARCHAR(255) NOT NULL,
+        nome VARCHAR(35) NOT NULL,
+        data_nascimento DATE NOT NULL,
+        diagnostico VARCHAR(50) NOT NULL,
+        
+	CONSTRAINT fk_serie_escolar
+    FOREIGN KEY (id_serie_escolar) REFERENCES tb_serie_escolar(id),
+    
+    CONSTRAINT fk_grau_suporte
+    FOREIGN KEY (id_grau_suporte)  REFERENCES tb_grau_suporte(id),
+    
+    CONSTRAINT fk_psicopedagogo
+    FOREIGN KEY (id_psicopedagogo) REFERENCES tb_psicopedagogo(id)
+        
+    );
+    
+    CREATE TABLE tb_responsavel_paciente(
+		
+        id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        
+        CONSTRAINT fk_responsavel
+		FOREIGN KEY (id_responsavel) REFERENCES tb_responsavel(id),
+        
+		CONSTRAINT fk_paciente
+		FOREIGN KEY (id_paciente) REFERENCES tb_paciente(id)
+    );
