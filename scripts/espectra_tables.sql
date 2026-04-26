c49f3f5e7f80e49d107852c7e741e0d889a10c96
GabrielPKTN <gabriellacerda20059@outlook.com>
2026-04-26 19:05:09 -0300

    fix: Arruma os scripts com o diagrama lógico atual

--- scripts/espectra_tables.sql
+++ scripts/espectra_tables.sql
@@ -1,4 +1,4 @@
- -- CREATE DATABASE db_espectra;
+CREATE DATABASE db_espectra;
 USE db_espectra;
 
 -- ----------------------------------
@@ -38,6 +38,7 @@ CREATE TABLE tb_grau_suporte(
 		
 	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 	grau INT NOT NULL
+    
 );
 
 CREATE TABLE tb_paciente(
@@ -50,13 +51,13 @@ CREATE TABLE tb_paciente(
     id_grau_suporte INT NOT NULL,
     id_psicopedagogo INT NOT NULL,
     
-    CONSTRAINT fk_serie_escolar
+    CONSTRAINT fk_serie_escolar_paciente
     FOREIGN KEY (id_serie_escolar) REFERENCES tb_serie_escolar(id),
     
-    CONSTRAINT fk_grau_suporte
+    CONSTRAINT fk_grau_suporte_paciente
     FOREIGN KEY (id_grau_suporte)  REFERENCES tb_grau_suporte(id),
     
-    CONSTRAINT fk_psicopedagogo
+    CONSTRAINT fk_psicopedagogo_paciente
     FOREIGN KEY (id_psicopedagogo) REFERENCES tb_psicopedagogo(id)
     
 );
@@ -88,61 +89,19 @@ CREATE TABLE tb_habilidade(
 
 );
 
-CREATE TABLE tb_atividade(
-	
-    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    id_habilidade INT NOT NULL,
-    id_status_atividade INT NOT NULL,
-    id_paciente INT NOT NULL,
-    
-    CONSTRAINT fk_habilidade
-    FOREIGN KEY (id_habilidade) REFERENCES tb_habilidade(id),
-    
-    CONSTRAINT fk_status_atividade
-    FOREIGN KEY (id_status_atividade)  REFERENCES tb_status_atividade(id),
-    
-    CONSTRAINT fk_paciente
-    FOREIGN KEY (id_paciente) REFERENCES tb_paciente(id)
-    
-    );    
-    
-CREATE TABLE tb_tipo_aplicacao(
-	
-    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    alternativa varchar(30) NOT NULL
-);
-
-CREATE TABLE tb_tentativa(
-
-	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    resultado BOOLEAN NOT NULL,
-    observacao VARCHAR(1500),
-    data_tentativa DATE NOT NULL,
-	id_tipo_aplicacao INT NOT NULL,
-    id_atividade INT NOT NULL,
-    
-    
-    CONSTRAINT fk_tipo_aplicacao
-    FOREIGN KEY (id_tipo_aplicacao)  REFERENCES tb_tipo_aplicacao(id),
-    
-	CONSTRAINT fk_atividade
-    FOREIGN KEY (id_atividade)  REFERENCES tb_atividade(id)
-
-);
-
 CREATE TABLE tb_atividade_personalizada(
 
 	id 	INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     questao VARCHAR(300) NOT NULL,
     valor_meses INT NOT NULL,
     id_psicopedagogo INT NOT NULL,
-    id_atividade INT NOT NULL,
+    id_habilidade INT NOT NULL,
     
     CONSTRAINT fk_psicopedagogo_atividade_personalizada
     FOREIGN KEY (id_psicopedagogo)  REFERENCES tb_psicopedagogo(id),
     
-	CONSTRAINT fk_atividade_atividade_personalizada
-    FOREIGN KEY (id_atividade)  REFERENCES tb_atividade(id)
+	CONSTRAINT fk_habilidade_atividade_personalizada
+    FOREIGN KEY (id_habilidade)  REFERENCES tb_habilidade(id)
     
 );
 
@@ -165,16 +124,62 @@ CREATE TABLE tb_atividade_portage(
         numero_questao INT NOT NULL,
         questao VARCHAR(300) NOT NULL,
         id_faixa_idade INT NOT NULL,
-		id_atividade INT NOT NULL,
+		id_habilidade INT NOT NULL,
         
         
 		CONSTRAINT fk_faixa_idade_atividade_portage
 		FOREIGN KEY (id_faixa_idade) REFERENCES tb_faixa_idade(id),
     
-		CONSTRAINT fk_atividade_atividade_portage
-		FOREIGN KEY (id_atividade) REFERENCES tb_atividade(id)
+		CONSTRAINT fk_habilidade_atividade_portage
+		FOREIGN KEY (id_habilidade) REFERENCES tb_habilidade(id)
         
     );
+    
+CREATE TABLE tb_atividade(
+	
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id_status_atividade INT NOT NULL,
+    id_paciente INT NOT NULL,
+    id_atividade_personalizada INT NULL,
+    id_atividade_portage INT NULL,
+    
+    CONSTRAINT fk_status_atividade_atividade
+    FOREIGN KEY (id_status_atividade)  REFERENCES tb_status_atividade(id),
+    
+    CONSTRAINT fk_paciente_atividade
+    FOREIGN KEY (id_paciente) REFERENCES tb_paciente(id),
+    
+    CONSTRAINT fk_atividade_personalizada_atividade
+    FOREIGN KEY (id_atividade_personalizada) REFERENCES tb_atividade_personalizada(id),
+    
+    CONSTRAINT fk_atividade_portage_atividade
+    FOREIGN KEY (id_atividade_portage) REFERENCES tb_atividade_portage(id)
+    
+    );    
+    
+CREATE TABLE tb_tipo_aplicacao(
+	
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    alternativa varchar(30) NOT NULL
+);
+
+CREATE TABLE tb_tentativa(
+
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    resultado BOOLEAN NOT NULL,
+    observacao VARCHAR(1500),
+    data_tentativa DATE NOT NULL,
+	id_tipo_aplicacao INT NOT NULL,
+    id_atividade INT NOT NULL,
+    
+    
+    CONSTRAINT fk_tipo_aplicacao_tentativa
+    FOREIGN KEY (id_tipo_aplicacao)  REFERENCES tb_tipo_aplicacao(id),
+    
+	CONSTRAINT fk_atividade_tentativa
+    FOREIGN KEY (id_atividade)  REFERENCES tb_atividade(id)
+
+);
 
 CREATE TABLE tb_formulario(
 		
