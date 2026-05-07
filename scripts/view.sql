3373b3b3c90f64aea08bcbc5400d7de4623d3fa4
NICOLAS DOS SANTOS DURÃO <25132912@dsn127m1241955.cfp127aluno.local>
2026-05-07 10:54:50 -0300

    feat: Adiciona view que retorna dados de uma tentativa pelo Id

--- scripts/view.sql
+++ scripts/view.sql
@@ -5,8 +5,6 @@
 -- VIEW QUE RETORNA INFORMAÇÕES DO FORMULÁRIO DE UM PACIENTE
 CREATE VIEW vw_formulario_paciente_id AS
 SELECT p.id AS id_paciente,
-JSON_OBJECT (
-	'questao',
     JSON_ARRAYAGG(
 		JSON_OBJECT(
 			'id', f.id,
@@ -21,15 +19,14 @@ JSON_OBJECT (
                 ),
 			'resposta', rf.alternativa
         )
-    )
-) AS resposta
+    ) AS questoes
 
 FROM tb_formulario f
 INNER JOIN tb_paciente p 
     ON p.id = f.id_paciente 
 INNER JOIN tb_atividade_portage ap 
     ON ap.id = f.id_atividade_portage
-INNER JOIN tb_resposta_formulario rf 
+LEFT JOIN tb_resposta_formulario rf 
     ON rf.id = f.id_resposta
 INNER JOIN tb_habilidade h 
     ON h.id = ap.id_habilidade
@@ -37,3 +34,74 @@ INNER JOIN tb_faixa_idade fi
     ON fi.id = ap.id_faixa_idade
     
 GROUP BY p.id;
+
+-- VIEW PARA RETORNAR O FORMULÁRIO DE UM PACIENTE PELO ID E SUAS RESPOSTAS
+CREATE VIEW vw_resposta_formulario_paciente_id AS
+SELECT p.id AS id_paciente,
+rf.alternativa AS resposta,
+	JSON_ARRAYAGG(
+		JSON_OBJECT(
+			'id', p.id,
+            'nome', p.nome,
+            'numero_questao', ap.numero_questao,
+            'questao', ap.questao,
+            'valor_atividade', ap.valor_atividade,
+            'habilidade', h.nome,
+				'faixa_idade', JSON_OBJECT(
+					'idade_min', fi.idade_min,
+                    'idade_max', fi.idade_max
+                ),
+			'resposta', rf.alternativa
+        )
+    ) AS questoes
+    
+FROM tb_formulario f
+INNER JOIN tb_paciente p 
+    ON p.id = f.id_paciente 
+INNER JOIN tb_atividade_portage ap 
+    ON ap.id = f.id_atividade_portage
+LEFT JOIN tb_resposta_formulario rf 
+    ON rf.id = f.id_resposta
+INNER JOIN tb_habilidade h 
+    ON h.id = ap.id_habilidade
+INNER JOIN tb_faixa_idade fi 
+    ON fi.id = ap.id_faixa_idade
+    
+GROUP BY p.id, rf.alternativa;
+
+-- VIEW PARA RETORNAR OS DADOS DE UMA TENTATIVA PELO ID
+CREATE VIEW vw_tentativa_id AS
+
+SELECT 
+    t.id AS id_tentativa,
+
+    JSON_OBJECT(
+        'id', t.id,
+        'resultado', t.resultado,
+        'data', t.data_tentativa,
+        'observacao', t.observacao,
+        'nivel_auxilio', ti.alternativa,
+
+        'atividade',
+
+        JSON_ARRAYAGG(
+            JSON_OBJECT(
+                'id', ap.id,
+                'questao', ap.questao,
+                'valor_meses', ap.valor_meses,
+                'psicopedagogo', p.nome
+            )
+        )
+
+    ) AS tentativa
+
+FROM tb_tentativa t
+
+INNER JOIN tb_tipo_aplicacao ti 
+    ON ti.id = t.id_tipo_aplicacao
+INNER JOIN tb_atividade_personalizada ap 
+    ON ap.id = t.id_atividade
+INNER JOIN tb_psicopedagogo p 
+    ON p.id = ap.id_psicopedagogo
+
+GROUP BY t.id;
